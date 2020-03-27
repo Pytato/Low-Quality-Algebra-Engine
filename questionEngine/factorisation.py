5d2b4d567cbacab3630dfd7d38a3842c6e071db4
Fwf <ffillingham@gmail.com>
2020-03-27 18:38:04 +0000

    Add completing the square question generation

--- questionEngine/factorisation.py
+++ questionEngine/factorisation.py
@@ -9,7 +9,7 @@ class FactorisationQuestions(BaseQuestionEngine):
 
     def gen_quad_factorisation_x_only(self, coeff_range=None, follower_range=None, **kwargs):
         if coeff_range is None:
-            coeff_range = [-6, 6]
+            coeff_range = [-2, 2]
         if follower_range is None:
             follower_range = [-12, 12]
         pre_gen_eq = 1 * \
@@ -18,11 +18,30 @@ class FactorisationQuestions(BaseQuestionEngine):
 
         return [latex(pre_gen_eq.expand()), latex(pre_gen_eq)]
 
+    def gen_quad_factorisation_complete_square_x_only(self, **kwargs):
+        coeff_range = [1, 3]
+        bracket_follower_range = [-6, 6]
+        external_follower_range = [-10, 10]
+        pre_gen_eq = random.randint(*coeff_range) * (
+            (random.randint(*coeff_range) * self.x + random.randint(*bracket_follower_range))**2 +
+            random.randint(*external_follower_range))
+
+        return [latex(pre_gen_eq.expand()), latex(pre_gen_eq)]
+
     def gen_question(self, diff=0, **kwargs):
         if diff == 0:
             return self.gen_quad_factorisation_x_only(**kwargs)
+        elif diff == 1:
+            return self.gen_quad_factorisation_complete_square_x_only(**kwargs)
+        elif diff == 4:
+            return random.choice([
+                self.gen_quad_factorisation_x_only,
+                self.gen_quad_factorisation_complete_square_x_only
+            ])(**kwargs)
 
 
 if __name__ == "__main__":
     test_fac_engine = FactorisationQuestions()
     print(test_fac_engine.gen_question_set(10, diff=0))
+    print(test_fac_engine.gen_question_set(10, diff=1))
+    print(test_fac_engine.gen_question_set(10, diff=4))
